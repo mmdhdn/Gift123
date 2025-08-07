b377d1ccea4ecb31ed683ddae78fcdb514ea79f1
k0d4dusha <skladmenov@mail.ru>
2025-08-07 21:38:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,10 @@ class GiftFlashBuyer:
             for session in session_list:
                 data_gifts = await session.get_available_gifts()    # чекаем новые подарки
                 self.checker_logger.info(f"Checked")
-                gifts_limit = [i.id for i in data_gifts if i.is_limited]   # создание списка лимитированных подарков для сравнения
+                if not is_test_mode:
+                    gifts_limit = [i.id for i in data_gifts if i.is_limited]   # создание списка лимитированных подарков для сравнения
+                else:
+                    gifts_limit = [i.id for i in data_gifts]
                 if len(gifts_limit) > len_old:   # если появились новые подарки
                     return data_gifts
             await asyncio.sleep(sleep_checking_seconds)
@@ -69,7 +72,7 @@ class GiftFlashBuyer:
             if not is_test_mode:
                 old_gifts = {i.id for i in old_gifts if i.is_limited}  # создание множество лимитированных подарков для сравнения
             else:
-                old_gifts = {i.id for i in old_gifts[:-2] if i.is_limited}  # test cache, 2 gifts
+                old_gifts = {i.id for i in old_gifts[2:]}  # test cache, 2 gifts
             self.checker_logger.info(f"Old limited gifts count (Cache): {len(old_gifts)}")
             self.checker_logger.info(f"Checking new gifts")
             data_gifts = await self.check_new_gifts(session_list, len(old_gifts))
@@ -77,10 +80,12 @@ class GiftFlashBuyer:
             for session in session_list:
                 await session.stop()
 
-
-        gifts_limit_dict = {gift.id: gift for gift in data_gifts if gift.is_limited}    # создания словаря из подарков для обращения к подарку по id
+        if not is_test_mode:
+            gifts_limit_dict = {gift.id: gift for gift in data_gifts if gift.is_limited}    # создания словаря из подарков для обращения к подарку по id
+        else:
+            gifts_limit_dict = {gift.id: gift for gift in data_gifts}
         self.new_gifts = [gifts_limit_dict[gift_id] for gift_id in gifts_limit_dict.keys() if gift_id not in old_gifts]     # отбираем новые подарки по id
-        self.new_gifts = [{"id": gift.id, "total": gift.total_amount, "price": gift.price} for gift in self.new_gifts]    # получаем саплай и цену для сортировки
+        self.new_gifts = [{"id": gift.id, "total": gift.total_amount or 0, "price": gift.price} for gift in self.new_gifts]    # получаем саплай и цену для сортировки
         self.new_gifts = sorted(self.new_gifts, key= lambda x:x['total'])   # сортируем по саплаю
         self.checker_logger.info(f"Found new {len(self.new_gifts)} gifts!")
 
